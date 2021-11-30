4e2a4a622a9922a0444005af092ca3db6df31a2a
Luma Rodrigues <44575153+luumarodrigues@users.noreply.github.com>
2021-11-30 14:21:32 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "Labs"
+  location = "eastus"
+}
+
+# Create the Linux App Service Plan
+resource "azurerm_app_service_plan" "appserviceplan" {
+  name                = "Demo-GithubActions"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
